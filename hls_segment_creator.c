cbd5aee41f843c09d918fe259214304dda47af7c
godka <asdic182@sina.com>
2015-07-19 08:17:03 +0800

    aspect ratio

--- hls_segment_creator.c
+++ hls_segment_creator.c
@@ -4,16 +4,19 @@
 typedef struct resolution{
 	int width;
 	int height;
-	int rate;
-	int maxrate;
+	unsigned int rate;
+	unsigned int maxrate;
 } Tresolution;
 
 Tresolution rlist [] = {
-	{ 1920, 1080, 1500, 2000 },
-	{ 1280, 720, 800, 1000 },
-	{ 640, 360, 400, 500 },
-	{ 320, 180, 200, 250 }
+	{ 0, 1080, 0, 0 },
+	{ 0, 720, 0, 0 },
+	{ 0, 480, 0, 0 },
+	{ 0, 360, 0, 0 }
 };
+
+#define TresolutionCount sizeof(rlist) / sizeof(Tresolution)
+
 int GetFileNameWithoutExtension(char* filename){
 	int i;
 	int tmp = strlen(filename);
@@ -24,6 +27,49 @@ int GetFileNameWithoutExtension(char* filename){
 	}
 	return 0;
 }
+
+int GetSystemOutput(char* cmdstring)
+{
+	char cmd[256] = {0};
+	sprintf(cmd, "ffmpeg -i \"%s\" 2> %s.log", cmdstring,cmdstring);
+	return system(cmd);
+}
+
+int GetFileDetail(const char* filename, int *width, int *height, int *bitrate){
+	int tmpwidth = 0, tmpheight = 0, tmpbitrate = 0,t,lockatt = 0;
+	char* p;
+	char tmp[1024] = { 0 };
+	char buffer[1024] = { 0 };
+	sprintf(tmp, "%s.log", filename);
+	FILE* ofile = fopen(tmp, "r");
+	while (!feof(ofile)){
+		fgets(tmp, 1024, ofile);
+		tmpwidth = 0, tmpheight = 0, tmpbitrate = 0;
+		char* p = strtok(tmp, ",");
+		while (p != NULL){
+			if (!lockatt)
+				t = sscanf(p, " %dx%d [%s] , %d kb/s", &tmpwidth, &tmpheight, buffer);
+			if (t == 3){
+				lockatt = 1;
+				t = 0;
+			}
+			else{
+				sscanf(p, "%d  kb/s", &tmpbitrate);
+			}
+			if (tmpwidth > 0 && tmpheight > 0 && tmpbitrate > 0){
+				*width = tmpwidth;
+				*height = tmpheight;
+				*bitrate = tmpbitrate;
+				fclose(ofile);
+				return 0;
+			}
+			p = strtok(NULL, ",");
+		}
+	}
+	fclose(ofile);
+	return 1;
+}
+
 int GetFileName(char* filename){
 	int i;
 	int tmp = strlen(filename);
@@ -43,7 +89,7 @@ int main(int argc, char* argv[])
 {
 	char* filename, *strformat;;
 	char tmp[1024] = { 0 }, realfilename[255] = { 0 },pfilename[255] = { 0 };
-	int realfileindex, pfileindex,i;
+	int realfileindex, pfileindex,i,oriwidth,oriheight,oribitrate,mostchecked;
 	FILE* istream; 
 #ifdef WIN32
 	strformat = "start ffmpeg -y -i %s -pix_fmt yuv420p -vcodec libx264 -acodec aac -strict -2 -r 25 -profile:v baseline  -b:v %dk -maxrate %dk -force_key_frames 50 -s %dx%d -map 0 -flags -global_header -f segment -segment_list %s_%d_%d.m3u8 -segment_time 10 -segment_format mpeg_ts -segment_list_type m3u8 %s_%d_%d_%%05d.ts";
@@ -52,31 +98,44 @@ int main(int argc, char* argv[])
 #endif
 	if (argc > 1){
 		filename = argv[1];
-		realfileindex = GetFileNameWithoutExtension(filename);
-		pfileindex = GetFileName(filename);
-		if (pfileindex - realfileindex > 0){
-			realfileindex = strlen(filename);
+		GetSystemOutput(filename);
+		if (GetFileDetail(filename, &oriwidth, &oriheight, &oribitrate) == 0){
+			realfileindex = GetFileNameWithoutExtension(filename);
+			pfileindex = GetFileName(filename);
+			if (pfileindex - realfileindex > 0){
+				realfileindex = strlen(filename);
+			}
+			memcpy(realfilename, filename + pfileindex, realfileindex - pfileindex);
+			memcpy(pfilename, filename + pfileindex, strlen(filename) - pfileindex);
+			puts(pfilename);
+			sprintf(tmp, "%s.m3u8", realfilename);
+			istream = fopen(tmp, "w");
+			fprintf(istream, "#EXTM3U\n");
+			/*auto hold code begin*/
+			for (i = 0; i < TresolutionCount; i++){
+				//aspect ratio
+				rlist[i].width = rlist[i].height * oriwidth / oriheight;
+				if (rlist[i].width % 2 == 1){
+					rlist[i].width++;
+				}
+				rlist[i].rate = (unsigned int) oribitrate * (unsigned int) rlist[i].width * (unsigned int) rlist[i].height / ((unsigned int) oriwidth * (unsigned int) oriheight);
+				rlist[i].maxrate = (unsigned int) rlist[i].rate *(unsigned int) 15 / (unsigned int) 10;
+				sprintf(tmp, strformat,
+					pfilename, rlist[i].rate, rlist[i].maxrate,
+					rlist[i].width, rlist[i].height,
+					realfilename, rlist[i].width, rlist[i].height,
+					realfilename, rlist[i].width, rlist[i].height);
+				fprintf(istream, "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=%d000\n", rlist[i].maxrate);
+				fprintf(istream, "%s_%d_%d.m3u8\n", realfilename, rlist[i].width, rlist[i].height);
+				system(tmp);
+			}
+			fprintf(istream, "#EXT-X-ENDLIST");
+			fclose(istream);
+			puts("all tasks done");
 		}
-		memcpy(realfilename, filename + pfileindex, realfileindex - pfileindex);
-		memcpy(pfilename, filename + pfileindex, strlen(filename) - pfileindex);
-		puts(pfilename);
-		sprintf(tmp, "%s.m3u8", realfilename);
-		istream = fopen(tmp, "w");
-		fprintf(istream, "#EXTM3U\n");
-		for (i = 0; i < sizeof(rlist) / sizeof(Tresolution); i++){
-			sprintf(tmp, strformat, 
-				pfilename,rlist[i].rate, rlist[i].maxrate,
-				rlist[i].width, rlist[i].height,
-				realfilename, rlist[i].width, rlist[i].height,
-				realfilename, rlist[i].width, rlist[i].height);
-			fprintf(istream, "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=%d000\n", rlist[i].maxrate);
-			fprintf(istream,"%s_%d_%d.m3u8\n", realfilename, rlist[i].width, rlist[i].height);
-				//execl(tmp,"ffmpeg",NULL);
-			system(tmp);
+		else{
+			printf("read file failed,cannot calc width and height.\n");
 		}
-		fprintf(istream, "#EXT-X-ENDLIST");
-		fclose(istream);
-		puts("done");
 	}
 	else{
 		puts("usage:hls_segment_creator filename");
