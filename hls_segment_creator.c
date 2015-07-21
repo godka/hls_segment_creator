518f68fdf47378fe15b346a029a5f4afdb5ab81d
godka <asdic182@sina.com>
2015-07-21 08:20:01 +0800

    -aspect_ratio

--- hls_segment_creator.c
+++ hls_segment_creator.c
@@ -9,14 +9,14 @@ typedef struct resolution{
 } Tresolution;
 
 Tresolution rlist [] = {
-	{ 0, 1080, 0, 0 },
-	{ 0, 720, 0, 0 },
-	{ 0, 480, 0, 0 },
-	{ 0, 360, 0, 0 }
+	{ 0, 1080, 1500, 2000 },
+	{ 0, 720, 800, 1000 },
+	{ 0, 480, 400, 500 },
+	{ 0, 360, 200, 300 }
 };
 
 #define TresolutionCount sizeof(rlist) / sizeof(Tresolution)
-
+int force_aspect_ratio = 0;
 int GetFileNameWithoutExtension(char* filename){
 	int i;
 	int tmp = strlen(filename);
@@ -98,48 +98,59 @@ int main(int argc, char* argv[])
 	strformat = "ffmpeg -y -i %s -pix_fmt yuv420p -vcodec libx264 -acodec aac -strict -2 -r 25 -profile:v baseline  -b:v %dk -maxrate %dk -force_key_frames 50 -s %dx%d -map 0 -flags -global_header -f segment -segment_list %s_%d_%d.m3u8 -segment_time 10 -segment_format mpeg_ts -segment_list_type m3u8 %s_%d_%d_%%05d.ts &";
 #endif
 	if (argc > 1){
-		filename = argv[1];
+		for (i = 1; i < argc; i++){
+			if (strcmp(argv[1], "-aspect_ratio") == 0){
+				force_aspect_ratio = 1;
+			}
+			else{
+				filename = argv[i];
+			}
+		}
 		GetSystemOutput(filename);
-		if (GetFileDetail(filename, &oriwidth, &oriheight, &oribitrate) == 0){
-			realfileindex = GetFileNameWithoutExtension(filename);
-			pfileindex = GetFileName(filename);
-			if (pfileindex - realfileindex > 0){
-				realfileindex = strlen(filename);
-			}
-			memcpy(realfilename, filename + pfileindex, realfileindex - pfileindex);
-			memcpy(pfilename, filename + pfileindex, strlen(filename) - pfileindex);
-			puts(pfilename);
-			sprintf(tmp, "%s.m3u8", realfilename);
-			istream = fopen(tmp, "w");
-			fprintf(istream, "#EXTM3U\n");
-			/*auto hold code begin*/
-			for (i = 0; i < TresolutionCount; i++){
-				//aspect ratio
-				rlist[i].width = rlist[i].height * oriwidth / oriheight;
-				if (rlist[i].width % 2 == 1){
-					rlist[i].width++;
+			if (GetFileDetail(filename, &oriwidth, &oriheight, &oribitrate) == 0){
+				for (i = 0; i < TresolutionCount; i++){
+					//aspect ratio
+					rlist[i].width = rlist[i].height * oriwidth / oriheight;
+					if (rlist[i].width % 2 == 1){
+						rlist[i].width++;
+					}
+					if (force_aspect_ratio){
+						rlist[i].rate = (unsigned long) oribitrate * (unsigned long) rlist[i].width / (unsigned long) oriwidth * (unsigned long) rlist[i].height / (unsigned long) oriheight;
+						rlist[i].maxrate = (unsigned long) rlist[i].rate *(unsigned long) 15 / (unsigned long) 10;
+					}
 				}
-				rlist[i].rate = (unsigned long) oribitrate * (unsigned long) rlist[i].width / (unsigned long) oriwidth * (unsigned long) rlist[i].height / (unsigned long) oriheight;
-				rlist[i].maxrate = (unsigned long) rlist[i].rate *(unsigned long) 15 / (unsigned long) 10;
-				sprintf(tmp, strformat,
-					pfilename, rlist[i].rate, rlist[i].maxrate,
-					rlist[i].width, rlist[i].height,
-					realfilename, rlist[i].width, rlist[i].height,
-					realfilename, rlist[i].width, rlist[i].height);
-				fprintf(istream, "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=%d000\n", rlist[i].maxrate);
-				fprintf(istream, "%s_%d_%d.m3u8\n", realfilename, rlist[i].width, rlist[i].height);
-				system(tmp);
 			}
-			fprintf(istream, "#EXT-X-ENDLIST");
-			fclose(istream);
-			puts("all tasks done");
+			else{
+				printf("read file failed,cannot calc width and height.\n");
+			}
+		realfileindex = GetFileNameWithoutExtension(filename);
+		pfileindex = GetFileName(filename);
+		if (pfileindex - realfileindex > 0){
+			realfileindex = strlen(filename);
 		}
-		else{
-			printf("read file failed,cannot calc width and height.\n");
+		memcpy(realfilename, filename + pfileindex, realfileindex - pfileindex);
+		memcpy(pfilename, filename + pfileindex, strlen(filename) - pfileindex);
+		puts(pfilename);
+		sprintf(tmp, "%s.m3u8", realfilename);
+		istream = fopen(tmp, "w");
+		fprintf(istream, "#EXTM3U\n");
+		/*auto hold code begin*/
+		for (i = 0; i < TresolutionCount; i++){
+			sprintf(tmp, strformat,
+				pfilename, rlist[i].rate, rlist[i].maxrate,
+				rlist[i].width, rlist[i].height,
+				realfilename, rlist[i].width, rlist[i].height,
+				realfilename, rlist[i].width, rlist[i].height);
+			fprintf(istream, "#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=%d000\n", rlist[i].maxrate);
+			fprintf(istream, "%s_%d_%d.m3u8\n", realfilename, rlist[i].width, rlist[i].height);
+			system(tmp);
 		}
+		fprintf(istream, "#EXT-X-ENDLIST");
+		fclose(istream);
+		puts("all tasks done");
 	}
 	else{
-		puts("usage:hls_segment_creator filename");
+		puts("usage:hls_segment_creator filename\n-aspect_ratio adaptive bitrate");
 	}
 #ifdef WIN32
 	getchar();
